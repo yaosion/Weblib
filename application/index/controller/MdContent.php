401d306c910458991136948a551976a97f1df932
HGZ <yaosion@qq.com>
2018-11-19 17:48:38 +0800

    联通三大基础页面，主页-列表页-内容页

--- application/index/controller/MdContent.php
+++ application/index/controller/MdContent.php
@@ -6,10 +6,17 @@ use think\Request;
 
 class MdContent extends Controller
 {
-    public function mdcontent($moretype_id)
-    {
+    public function mdcontent($moretype_id,$type_id)
+    {	
+    	$contentTypeLi='';
+    	$list = Db::name('moretype')->where('type_id',$type_id)->select();
     	$data = Db::name('md')->where('md_id',$moretype_id)->find();
+    	foreach ($list as $list) {
+        	$contentTypeLi .= '<li class="sidebar-nav-item"><a class="js-scroll-trigger" href="/index/md_content/mdcontent/moretype_id/'.$list['moretype_id'].'/type_id/'.$type_id.'">'.$list['moretype_name'].'</a></li>';
+        };
+
     	$this->assign('data',$data);
+    	$this->assign('contentTypeLi',$contentTypeLi);
     	return $this->fetch('MdContent/mdcontent');
     }
     
