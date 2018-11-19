727347a2f65c9daeeff66d31d21a94fe92ee3822
HGZ <yaosion@qq.com>
2018-11-19 18:01:56 +0800

    改变内容检索条件

--- application/index/controller/MdContent.php
+++ application/index/controller/MdContent.php
@@ -6,13 +6,13 @@ use think\Request;
 
 class MdContent extends Controller
 {
-    public function mdcontent($moretype_id,$type_id)
+    public function mdcontent($moretype_name)
     {	
     	$contentTypeLi='';
-    	$list = Db::name('moretype')->where('type_id',$type_id)->select();
-    	$data = Db::name('md')->where('md_id',$moretype_id)->find();
+    	$data = Db::name('md')->where('md_typename',$moretype_name)->find();
+    	$list = Db::name('moretype')->where('type_id',$data['md_typeid'])->select();
     	foreach ($list as $list) {
-        	$contentTypeLi .= '<li class="sidebar-nav-item"><a class="js-scroll-trigger" href="/index/md_content/mdcontent/moretype_id/'.$list['moretype_id'].'/type_id/'.$type_id.'">'.$list['moretype_name'].'</a></li>';
+        	$contentTypeLi .= '<li class="sidebar-nav-item"><a class="js-scroll-trigger" href="/index/md_content/mdcontent/moretype_name/'.$list['moretype_name'].'">'.$list['moretype_name'].'</a></li>';
         };
 
     	$this->assign('data',$data);
