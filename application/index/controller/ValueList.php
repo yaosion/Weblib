471efa9ff092427d7079ac62305209b2f76c3f62
HGZ <yaosion@qq.com>
2018-11-19 09:53:57 +0800

    检索列表样式

--- application/index/controller/ValueList.php
+++ application/index/controller/ValueList.php
@@ -9,11 +9,26 @@ class ValueList extends Controller
     public function valuelist($type_id)
     {	
     	$moreTypeLi = '';
+    	$listValue = '';
     	$list = Db::name('moretype')->where('type_id',$type_id)->select();
+    	$typeName = Db::table('type')->where('type_id',$type_id)->find();
         foreach ($list as $list) {
         	$moreTypeLi .= '<li class="sidebar-nav-item"><a class="js-scroll-trigger" href="/index/md_content/mdcontent/moretype_id/'.$list['moretype_id'].'">'.$list['moretype_name'].'</a></li>';
+        	$listValue .=  '<div class="col-lg-3">
+        						<a class="portfolio-item" href="/index/md_content/mdcontent/moretype_id/'.$list['moretype_id'].'">
+	        	              		<span class="caption">
+	        	                	<span class="caption-content">
+	        	                  	<h2>'.$list['moretype_name'].'</h2>
+	        	                  	<p class="mb-0"></p>
+	        	                	</span>
+	        	              		</span>
+	        	              		<img class="img-fluid" src="/static/images/listbg.jpg" alt="">
+        	            		</a>
+        	            	</div>';
         };
+        $this->assign('typeName',$typeName);
     	$this->assign('moreTypeLi',$moreTypeLi);
+    	$this->assign('listValue',$listValue);
     	return $this->fetch('ValueList/valuelist');
     }
     
