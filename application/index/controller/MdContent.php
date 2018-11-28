9692b45b4564c780b0b31b3d453c3e13c377a85a
HGZ <yaosion@qq.com>
2018-11-28 14:49:18 +0800

    搜索功能完善

--- application/index/controller/MdContent.php
+++ application/index/controller/MdContent.php
@@ -10,9 +10,11 @@ class MdContent extends Controller
     	$contentTypeLi='';
     	$data = Db::name('md')->where('md_typename',$moretype_name)->find();
     	$list = Db::name('moretype')->where('type_id',$data['md_typeid'])->select();
+    	if($data != ''){
     	foreach ($list as $list) {
         	$contentTypeLi .= '<li class="sidebar-nav-item"><a class="js-scroll-trigger" href="/index/md_content/mdcontent/moretype_name/'.$list['moretype_name'].'">'.$list['moretype_name'].'</a></li>';
         };
+    	};
 
     	$this->assign('data',$data);
     	$this->assign('contentTypeLi',$contentTypeLi);
--- application/index/controller/SearchList.php
+++ application/index/controller/SearchList.php
@@ -9,8 +9,29 @@ class SearchList extends Controller
 {
     public function searchlist($searchValue)
     {
-        $value = $searchValue;
-        $this->assign('value',$value);
+        $_key = $searchValue;
+        $_searchValue = '';
+        $_value = Db::table('md')
+            ->where('md_typename|md_title','like','%'.$_key.'%')
+            ->select();
+        if($_value != ''){
+            foreach ($_value as $_value) {
+                $_searchValue .=  '<div class="col-lg-3">
+		        						<a class="portfolio-item" href="/index/md_content/mdcontent/moretype_name/'.$_value['md_typename'].'">
+			        	              		<span class="caption">
+			        	                	<span class="caption-content">
+			        	                  	<h2>'.$_value['md_typename'].'</h2>
+			        	                  	<p class="mb-0"></p>
+			        	                	</span>
+			        	              		</span>
+			        	              		<img class="img-fluid" src="/static/images/listbg.jpg" alt="">
+		        	            		</a>
+		        	            	</div>';
+            };
+        }else{
+            return ;
+        };
+        $this->assign('searchValue',$_searchValue);
         return $this->fetch('SearchList/searchlist');
     }
 }
