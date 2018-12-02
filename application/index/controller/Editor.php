d57be67fec19542bed4a02e0df1204310fdb7fd3
HGZ <yaosion@qq.com>
2018-12-02 17:35:59 +0800

    编辑器数据获取

--- application/index/controller/Editor.php
+++ application/index/controller/Editor.php
@@ -4,82 +4,31 @@ namespace app\index\controller;
 
 use think\Controller;
 use think\Request;
-
+use think\Db;
 class Editor extends Controller
 {
-    /**
-     * 显示资源列表
-     *
-     * @return \think\Response
-     */
-    public function index()
+    public function  editor()
     {
-        //
+        $type = Db::name('type')->select();
+        $typeData = [
+            'type' => $type
+        ];
+        $this->assign($typeData);
+        return $this->fetch('Editor/editor');
     }
 
-    /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
-    public function create()
+    public function add()
     {
-        //
-    }
-
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-        //
-    }
-
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
-
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
+        if(request()->isAjax()){
+            $data = [
+                'md_content' => input('md_content'),
+                'md_mdcontent' => input('md_mdcontent'),
+                'md_time' => input('md_time'),
+                'md_title' => input('md_title'),
+                'md_typeid' => input('md_typeid'),
+                'md_typename' => input('md_typename'),
+            ];
+            return $data;
+        }
     }
 }
