b4d4743fe207c2a680884c11214cc109fe59c62a
HGZ <yaosion@qq.com>
2018-12-10 19:48:26 +0800

    重编辑保存

--- application/index/model/ReEditor.php
+++ application/index/model/ReEditor.php
@@ -15,8 +15,8 @@ class ReEditor extends Model
         if(Db::name('md')->where('md_id',$data['md_id'])->find()){
             $result=Db::name('md')
                 ->where('md_id', $data['md_id'])
-                ->update(['md_content' => $data['md_content']],['md_mdcontent' => $data['md_mdcontent']],['md_time' => $data['md_time']],['md_title' => $data['md_title']],['md_typeid' => $data['md_typeid']],['md_typename' => $data['md_typename']]);
-            if($result){
+                ->update($data);
+            if($result != 0){
                 return 1;
             }else{
                 return '修改失败';
