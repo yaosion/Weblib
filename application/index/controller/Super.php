0011b32d3a479f91dea36430bb7701dc455478f4
HGZ <yaosion@qq.com>
2018-12-26 15:12:23 +0800

    重写后台管理

--- application/index/controller/Super.php
+++ application/index/controller/Super.php
@@ -11,6 +11,10 @@ class Super extends Controller
 {
     public function super()
     {
+        return $this->fetch('Super/super');
+    }
+
+    public function getMd(){
         //判断Session是否有账号
         //@userInfo 返回‘10’ 没有登陆账号
         $_userId = '';
@@ -20,17 +24,42 @@ class Super extends Controller
             $_userInfo = Session::get('userInfo');
             $_userId = $_userInfo['id'];
         };
+        //获取分页page和limit参数
+        $page=input("get.page")?input("get.page"):1;
+        $page=intval($page);
+        $limit=input("get.limit")?input("get.limit"):1;
+        $limit=intval($limit);
+        $start=$limit*($page-1);
+        //分页查询
         if($_userId == 1){
-            $allMd = Db::name('md')->select();
+            //分页查询
+            $count=Db::name("md")->count("md_id");
+            $allMd = Db::name('md')->limit($start,$limit)->order('md_id desc')->select();
+            $list["msg"]="";
+            $list["code"]=0;
+            $list["count"]=$count;
+            $list["data"]=$allMd;
+            if(empty($cate_list)){
+                $list["msg"]="暂无数据";
+            }
+            //将对象转换json返回
+            return json($list);
         }else{
-            $allMd = Db::name('md')->where('md_belongs',$_userId)->select();
+            //分页查询
+            $count=Db::name("md")->count("md_id");
+            $allMd = Db::name('md')->where('md_belongs',$_userId)->limit($start,$limit)->order('md_id desc')->select();
+            $list["msg"]="";
+            $list["code"]=0;
+            $list["count"]=$count;
+            $list["data"]=$allMd;
+            if(empty($cate_list)){
+                $list["msg"]="暂无数据";
+            }
+            //将对象转换json返回
+            return json($list);
         }
-        $valueAssign =[
-            'allMd' => $allMd,
-        ];
-        $this->assign($valueAssign);
-        return $this->fetch('Super/super');
     }
+
     public function mdDelete()
     {
         if(request()->isAjax()){
